703c18e62bd29ac2d761fade93b7bd115ad8f464
Enning Yang <33254423+zihuaihuai@users.noreply.github.com>
2025-02-27 13:15:19 -0500

    change wet run dir

--- micapipe/wet_run.sh
+++ micapipe/wet_run.sh
@@ -1,1 +1,1 @@
-./run.py /data_/mica3/BIDS_CI/rawdata /home/bic/eyang/processed_data2 participant --force-output --participant-label mri3T --cores 15
+./run.py /data_/mica3/BIDS_CI/rawdata /data/mica1/03_projects/enning/snakebids_test participant --force-output --participant-label mri3T --cores 15
